9216a3d1d727decad3af6cea73719f507c93e6ee
Ahsan Irfan <ahsan.irfan@gmail.com>
2022-02-09 21:15:17 +0000

    added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+// EQARRAYS FUNCTION TO BE CALLED BY THE ASSERTEQUALARRAYS FUNCTION BELOW
+const eqArrays = function(arrayOne, arrayTwo) {
+  
+  if (arrayOne.length !== arrayTwo.length) {
+    return false
+  }
+
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;        
+    }
+  }
+  
+  return true;
+};
+
+// ASSERTARRAYSEQUAL
+const assertArraysEqual = function(actualArray, expectedArray) {
+  let finalMessage = `✅✅✅ Assertion With Array Output Passed: ${actualArray} === ${expectedArray}`;
+
+  if (eqArrays(actualArray, expectedArray) === false) {
+    finalMessage = `🛑🛑🛑 Assertion With Array Output Failed: ${actualArray} !== ${expectedArray}`;
+  }
+  console.log(finalMessage);
+};
+
+// LETTERPOSITIONS FUNCTION
+const letterPositions = function(sentence) {
+  const results = {};
+    for (let i = 0; i < sentence.length; i++) {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  return results;
+};
+
+// TEST CASE : HELLO
+
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2,3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
