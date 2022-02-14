f77b287ae4c7f930c9199eda216fede47dce94e0
Ahsan Irfan <ahsan.irfan@gmail.com>
2022-02-14 16:05:26 +0000

    made edits to tailTest, middleTest & eqArrays

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,6 +3,6 @@ const tail = require('../tail');
 
 describe("#tail", () => {
   it("returns [2, 3, 4] for [1, 2, 3, 4]", () => {
-    assert.sameOrderedMembers(tail([1, 2, 3, 4]), [2, 3, 4]);
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
   });
 });
